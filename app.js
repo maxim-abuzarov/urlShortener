a8aa6df1d0c2ef86b53839ab5c2ec36b2302a6e8
Abuzarov Maxim <maxim.abuzarov@gmail.com>
2021-05-30 16:51:24 +0700

    add auth/login routes, validation for email/password

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const mongoose = require('mongoose')
 
 const app = express()
 
+app.use('/api/auth', require('./routes/auth.routes'))
+
 const PORT = config.get('port') || 5000
 
 async function start() {
@@ -14,7 +16,8 @@ async function start() {
             useCreateIndex: true
         })
         app.listen(PORT, () => console.log(`App has been started on port ${PORT}`))
-    }   catch (e) {
+    }
+    catch (e) {
         console.log('Server error', e.message)
         process.exit(1)
     }
